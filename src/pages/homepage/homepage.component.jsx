a2423bbeda21b7b2ac7946052b4cae73cded3de6
Paolo Sax <sax.paolino@gmail.com>
2020-09-23 10:35:08 +0200

    Homepage and Directory Components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+import Directory from '../../components/directory/directory.component';
+
+// creo funct component per la home page, a questo punto non mi servono state e lifecycle meth
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
