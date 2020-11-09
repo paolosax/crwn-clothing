e7dec9a971878c12fa66c41f9f67fc050ac90c34
Paolo Sax <sax.paolino@gmail.com>
2020-11-09 16:42:44 +0100

    mapDispatchToProps - "Added redux and implemented userReducer and userAction to header and app components"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -10,11 +11,14 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
+import { setCurrentUser } from './redux/user/user.action';
+
 class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     // se un utente si iscrive...
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       // verifichiamo se effettivamente si sta iscrivendo (if(userAuth)...)
@@ -31,17 +35,15 @@ class App extends React.Component {
         // ma prendiamo anche il primo stato di quei dati
         // quindi facciamo il setState della nostra app in js
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
       } else {
         // se l'utente fa logout, impostiamo il currentUser su null
         // che riceviamo dalla library auth
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     })
   }
@@ -64,4 +66,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
