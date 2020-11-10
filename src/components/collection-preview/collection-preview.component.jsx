e1bb00155ef9d115d3f6c59fa4b799e6ea27a7f8
Paolo Sax <sax.paolino@gmail.com>
2020-11-10 21:37:07 +0100

    Cart Item Reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React from 'react';
 
-import "./collection-preview.styles.scss";
+import './collection-preview.styles.scss';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
       {/* 
         nota (lezione 74): ricordarsi che allo stato attuale, che ogni volta che il component
         CollectionPreview viene renderizzato, la chimata alla funzione con il loop viene 
         chiamata ogni volta e potrebbe comportare un costo in termini di prestazioni.
         Vedremo come gestire la cosa più tardi
-      */}    
+      */}
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
-  </div>  
+  </div>
 );
 
 export default CollectionPreview;
